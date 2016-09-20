59e6ec0bc49ab6b1064891ab462780646b5605da
Thomas Clippr McCloskey <mccloskeytc@dcsdk12.org>
2016-09-20 12:56:43 -0600

    yee

--- string_2.py
+++ string_2.py
@@ -25,3 +25,28 @@ def cat_dog(str):
       count_cat += 1
   return count_cat == count_dog
 print cat_dog("catdog")
+
+
+def count_code(str):
+  num = 0
+  for i in range(0, len(str)-3):
+    if str[i:i+2] == 'co' and str[i+3] == 'e':
+      num += 1
+  return num
+print count_code("may may code meme code code")
+
+
+def end_other(a, b):
+  a = a.lower()
+  b = b.lower()
+  return a[-(len(b)):] == b or a == b[-(len(a)):]
+print end_other("meme", "meme")
+
+
+def xyz_there(str):
+  for i in range(len(str)):
+    if str[i] != '.' and str[i+1:i+4] == 'xyz':
+      return True
+  if str[0:3] == 'xyz':
+    return True
+  return False
